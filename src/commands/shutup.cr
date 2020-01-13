0eb5fd0cbe3251889f6731410ba4dfd064c92411
Giorgi Kavrelishvili <44188195+grkek@users.noreply.github.com>
2020-01-13 10:51:31 +0400

    Update shutup.cr
    - Making the scope so small will result in chaos because there is a larger chance to catch a 2 in 4 range than catch a 16 in 33 range.

--- src/commands/shutup.cr
+++ src/commands/shutup.cr
@@ -3,7 +3,7 @@ module Cryb
     BOT.on_message_create do |payload|
       next if payload.author.bot || payload.content.starts_with? CONFIG.prefix
       
-      if Random.rand(0..3) == 2
+      if Random.rand(0..32) == 16
         BOT.create_message(payload.channel_id, "<@#{payload.author.id}> Shutup")
       end
     end
